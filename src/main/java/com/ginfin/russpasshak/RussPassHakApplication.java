3f62fe5083124099ac2d77a1ac10d69a03055f7e
ginfin <evgenii2148@gmail.com>
2024-03-27 17:18:48 +0300

    init

--- src/main/java/com/ginfin/russpasshak/RussPassHakApplication.java
+++ src/main/java/com/ginfin/russpasshak/RussPassHakApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RusspasshakApplication {
+public class RussPassHakApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RusspasshakApplication.class, args);
+		SpringApplication.run(RussPassHakApplication.class, args);
 	}
 
 }
