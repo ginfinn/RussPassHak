e4eb9cb928926e60df4a9202ece431ce3b69d4f8
ginfin <evgenii2148@gmail.com>
2024-03-27 17:17:18 +0300

    inint

---
+++ src/main/java/com/ginfin/russpasshak/RusspasshakApplication.java
@@ -0,0 +1,13 @@
+package com.ginfin.russpasshak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RusspasshakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RusspasshakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ginfin/russpasshak/RusspasshakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ginfin.russpasshak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RusspasshakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
